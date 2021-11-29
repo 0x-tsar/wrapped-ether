2d1c8319a055f13eb0fc1e067e7b31162532eda7
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:28:09 -0300

    :sparkles:

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,5 +3,5 @@ const WrappedEther = artifacts.require("WrappedEther");
 module.exports = async (deployer, networks, accounts) => {
   await deployer.deploy(WrappedEther);
   const wrappedEther = await WrappedEther.deployed();
-  console.log(wrapped);
+  console.log(wrappedEther);
 };
