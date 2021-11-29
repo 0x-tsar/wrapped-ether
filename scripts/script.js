652148fe1ac55ea1305b4896d034e6582f01b79b
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:53:06 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,26 @@ module.exports = async () => {
 
     const wrappedEther = await WrappedEther.deployed();
 
-    const totalWether = await wrappedEther.totalSupplyWether();
-    console.log(parseInt(totalWether));
+    // const totalWether = await wrappedEther.totalSupplyWether();
+    // console.log(parseInt(totalWether));
 
-    // let myWeth = await wrappedEther.balanceOf(account);
-    // console.log(`before: ${myWeth}`);
+    let myWeth = await wrappedEther.balanceOf(account);
+    console.log(`before: ${myWeth}`);
 
-    // const value = web3.utils.toWei("0.1");
-    // await wrappedEther.mint({ from: account, value: value });
-    // // await wrappedEther.mint().send({ from: account, value: value });
-    // console.log(myWeth);
+    const value = web3.utils.toWei("0.1");
+    await wrappedEther.mint({ from: account, value: value });
+    // await wrappedEther.mint().send({ from: account, value: value });
+    console.log(myWeth);
 
-    // myWeth = await wrappedEther.balanceOf(account);
-    // console.log(`after: ${myWeth}`);
+    myWeth = await wrappedEther.balanceOf(account);
+    console.log(`after: ${myWeth}`);
+
+    console.log(`NOW WITHDRAW IT`);
+
+    await wrappedEther.withdraw(value, { from: account });
+
+    myWeth = await wrappedEther.balanceOf(account);
+    console.log(`after withdraw WETH AMOUNT: ${myWeth}`);
 
     //
   } catch (error) {
