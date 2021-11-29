a22720688d29d910476226951fec6ff63a4587ab
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:41:01 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,12 @@ const WrappedEther = artifacts.require("WrappedEther");
 
 module.exports = async () => {
   try {
-    const [account, _] = await web3.utils.getAccounts();
+    const [account, _] = await web3.eth.getAccounts();
     console.log(account);
 
     const wrappedEther = await WrappedEther.deployed();
-    console.log(wrappedEther);
+
+    let myWeth = await wrappedEther.methods.balanceOf(account);
 
     console.log(wrappedEther.methods);
   } catch (error) {
