54f97c090b082e991ce8466d263dbf85b3f4833a
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:38:14 -0300

    :sparkles:

--- contracts/WrappedEther.sol
+++ contracts/WrappedEther.sol
@@ -4,13 +4,22 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WrappedEther is ERC20 {
+    uint256 public totalSupplyWether = 0;
+
     constructor() ERC20("WRAPPED ETHER", "WETH") {}
 
     function mint() public payable {
         _mint(msg.sender, msg.value);
+        totalSupplyWether += msg.value;
     }
 
     function withdraw(uint256 amount) public {
-        require(amount <= balanceOf(msg.sender));
+        require(
+            amount <= balanceOf(msg.sender),
+            'You don"t own that amount of Ethers'
+        );
+
+        payable(msg.sender).transfer(amount);
+        totalSupplyWether -= amount;
     }
 }
