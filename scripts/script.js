6396c7bd9021185d6fbc2884da060500df94c741
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:42:06 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,11 @@ module.exports = async () => {
     const wrappedEther = await WrappedEther.deployed();
 
     let myWeth = await wrappedEther.methods.balanceOf(account);
+    console.log(myWeth);
+
+    const value = web3.utils.toWei("0.1");
+    await wrappedEther.methods.mint().send({ from: account, value: value });
+    console.log(myWeth);
 
     console.log(wrappedEther.methods);
   } catch (error) {
