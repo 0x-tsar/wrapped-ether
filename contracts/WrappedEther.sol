6ddf9b9e9e476fed07e300a0b004e52131c428d2
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:23:32 -0300

    :sparkles:

--- contracts/WrappedEther.sol
+++ contracts/WrappedEther.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract WrappedEther {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WrappedEther is ERC20 {
+    constructor() ERC20("WRAPPED ETHER", "WETH") {
+        mint(1 * 10**18);
+    }
 }
