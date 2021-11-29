339c1d20f6c69d18b037131fe91f16ed74c940f8
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 06:55:17 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -10,16 +10,16 @@ module.exports = async () => {
     // const totalWether = await wrappedEther.totalSupplyWether();
     // console.log(parseInt(totalWether));
 
-    let myWeth = await wrappedEther.balanceOf(account);
-    console.log(`before: ${myWeth}`);
+    // let myWeth = await wrappedEther.balanceOf(account);
+    // console.log(`before: ${myWeth}`);
 
     const value = web3.utils.toWei("0.1");
-    await wrappedEther.mint({ from: account, value: value });
-    // await wrappedEther.mint().send({ from: account, value: value });
-    console.log(myWeth);
+    // await wrappedEther.mint({ from: account, value: value });
+    // // await wrappedEther.mint().send({ from: account, value: value });
+    // console.log(myWeth);
 
-    myWeth = await wrappedEther.balanceOf(account);
-    console.log(`after: ${myWeth}`);
+    // myWeth = await wrappedEther.balanceOf(account);
+    // console.log(`after: ${myWeth}`);
 
     console.log(`NOW WITHDRAW IT`);
 
